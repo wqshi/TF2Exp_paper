7314d6f551b55a850074dcef02c08c15bdfcb90d
wqshi <wqshi.nudt@gmail.com>
2016-03-01 15:51:41 -0800

    Before the chrTF

--- R/s_summary_regression_results.R
+++ R/s_summary_regression_results.R
@@ -8,11 +8,9 @@ setwd('~/expression_var/R/')
 source('~/R/s_function.R', chdir = TRUE)
 source('s_gene_regression_fun.R')
 source('s_summary_fun.R')
-library(doMC)
 library(stringr)
 #install.packages('doMC')
-
-library(GenomicRanges)
+library(dplyr)
 library("optparse")
 
 option_list = list(
@@ -27,42 +25,74 @@ if (is.null(opt$batch_name)){
     #batch_name = '54samples_evalue'
     #batch_name = '462samples_sailfish_quantile'
     #batch_name = '462samples_snyder_norm'
-    batch_name = '462samples_quantile'
-    #batch_name = '462samples_quantile_rmNA'
-    chr_str = 'chr22'
+    #batch_name = '462samples_quantile'
+    batch_name = '462samples_quantile_rmNA'
+    #chr_str = 'chr22'
+    chr_str = 'chrTF'
     
 }else{
     batch_name = opt$batch_name
     chr_str = opt$chr
 }
 
+mode_name = 'glmnet'
+
+f_summary_regression_results <- function(batch_name, chr_str, mode_name){
+    output_dir = f_p("./data/%s/rnaseq/%s/%s/", batch_name, chr_str, mode_name)
+
+    #Rsync the features files back to the clustdell
+    rsync_cmd = f_p("rsync -rav  --include '*enet*' --exclude '*' shi@clustdell.cmmt.ubc.ca:/home/shi/expression_var/data/%s/rnaseq/%s/%s/   ~/expression_var/data/%s/rnaseq/%s/%s/", batch_name, chr_str, mode_name, batch_name, chr_str, mode_name)
+    system(rsync_cmd)
+
+    
+    
+    features_df=f_merge_data(output_dir, '.*enet.feature', quite = FALSE)
+    
+    performance_df = f_merge_data(output_dir, '.*enet$')
+    head(performance_df)
+
+    head(features_df)
+
+    head(performance_df)
+    cat('Mean performance:',mean(performance_df$performance), '\n')
+    hist(performance_df$performance)
 
 
-output_dir = f_p("./data/%s/rnaseq/%s/", batch_name, chr_str)
-
-#Rsync the features files back to the clustdell
-rsync_cmd = f_p("rsync -rav  --include '*enet*' --exclude '*' shi@clustdell.cmmt.ubc.ca:/home/shi/expression_var/data/%s/rnaseq/%s/   ~/expression_var/data/%s/rnaseq/%s/", batch_name, chr_str,batch_name, chr_str)
-system(rsync_cmd)
+    good_predictions <- performance_df %>% filter(performance > 0.5)
+    
+    cat(nrow(good_predictions), 'out of', nrow(performance_df), 'have good predictions(Rsquared > 0.5)','\n')
 
 
+    write.table(performance_df, f_p('%s/performance', output_dir), quote =FALSE, sep = '\t', row.names= FALSE)
+    return (performance_df)
+}
 
-features_df=f_merge_data(output_dir, '.*enet.feature')
-performance_df = f_merge_data(output_dir, '.*enet$')
-head(performance_df)
-
-head(features_df)
-
-head(performance_df)
-cat('Mean performance:',mean(performance_df$performance), '\n')
-hist(performance_df$performance)
-
-library(dplyr)
-good_predictions <- performance_df %>% filter(performance > 0.4)
-
-cat(nrow(good_predictions), 'out of', nrow(performance_df), 'have good predictions(Rsquared > 0.4)','\n')
+batch_list = c('462samples_quantile', '462samples_quantile_rmNA', '462samples_var_quantile', '462samples_snyder_norm')
+for (batch_name in batch_list){
+    cat('======', batch_name, '========\n')
+    #performance = f_summary_regression_results(batch_name, chr_str, mode_name)
+    Sys.sleep(1)
+}
 
 
-write.table(performance_df, f_p('%s/performance', output_dir), quote =FALSE, sep = '\t', row.names= FALSE)
+mode_list = c('add.histone_add.miRNA_add.TF.exp_model.glmnet_add.permutation',
+    'add.histone_add.miRNA_add.TF.exp_model.glmnet_rm.permutation',
+    'add.histone_add.miRNA_rm.TF.exp_model.glmnet_rm.permutation',
+      'add.histone_rm.miRNA_add.TF.exp_model.glmnet_rm.permutation',
+    'add.histone_add.miRNA_add.TF.exp_model.glmnet_add.permutation_add.TF.exp.only'
+    )
+
+mode_list =c('add.histone_add.miRNA_rm.TF.exp_model.glmnet_rm.permutation_rm.TF.exp.only',
+    'add.histone_add.miRNA_rm.TF.exp_model.glmnet_rm.permutation_add.TF.exp.only')
+
+
+
+for (loc_mode in mode_list){
+    cat('======', loc_mode, '========\n')
+    performance = f_summary_regression_results('462samples_quantile_rmNA', chr_str, loc_mode)
+    Sys.sleep(1)
+}
+
 
 
 stop()
