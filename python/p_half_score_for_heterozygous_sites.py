a7ec0590608f7c697d3ffb72a59d594b1040e581
wqshi <wqshi.nudt@gmail.com>
2016-03-10 12:08:44 -0800

    finish the unit test of half score

--- python/p_half_score_for_heterozygous_sites.py
+++ python/p_half_score_for_heterozygous_sites.py
@@ -35,48 +35,94 @@ tf_dir = '%s/data/raw_data/tf/encode_peaks/' % project_dir
 deepsea_dir = '%s/deep_result/%s/%s/evalue/' % (batch_output_dir, mode_str ,chr_num)
 vcf_dir = '%s/chr_vcf_files/%s/%s/' % (batch_output_dir, mode_str ,chr_num)
 
-my.f_ensure_make_dir('%s/het/'%deepsea_dir)
 
-variation_file_list = my.f_shell_cmd( "find %s -name '*.diff.gz'"%(deepsea_dir), quiet = True).split('\n')[0:-1]
 
-for variation_file in variation_file_list:
+def f_half_score_het_sites(variation_file, deepsea_dir):
     sample_id = os.path.basename(variation_file).split('.')[0]
     variation_data = pd.read_csv(variation_file, sep =',', compression= 'gzip').dropna()
     print sample_id
     #print variation_data.ix[1105:1110,1:6].head()
     genotype_data=variation_data[['name']]
 
-    print genotype_data.shape
+    #print genotype_data.shape
     #print pd.isnull(variation_data).any(1).nonzero()[0]
     #print genotype_data[:5]
     assert len(genotype_data) == variation_data.shape[0], 'Size of genotype data not match'
 
     
     pattern = re.compile('(0.1|1.0)')
-
-    
     het_selection = [ re.match(pattern, genotype) is not None  for genotype in genotype_data.ix[:,'name'].tolist() ]
 
-    print het_selection
-    print variation_data.shape
-    print len(het_selection)
-    
-    i = 0;
-    #genotype_data[1107]
-    #for genotype in genotype_data.ix[:,'name'].tolist():
-    #    i = i + 1;
-    #    re.match(pattern, genotype)
-    #    print i
+    assert len(het_selection) == variation_data.shape[0], 'Het selection is different from the variation_data'
 
-    
-    #print het_selection[0:5]
     numeric_cols =my.grep_list('GM12878', variation_data.columns)
     
     het_variation = variation_data.copy()
     het_variation.ix[het_selection, numeric_cols] = 0.5 * variation_data.ix[het_selection, numeric_cols]
 
     het_variation.to_csv( "%s/het/%s.diff"%(deepsea_dir, sample_id), index = False, sep = ',')
+
+
+
+###Part 2. Correct the bias of GM12878
+
+
+
+
+
+import unittest
+class TestDatabaseTable(unittest.TestCase):
+    def setUp(self):
+        a = 0
+        
+    def test_half_score(self):
+        variation_file_list = my.f_shell_cmd( "find %s -name '*.diff.gz'"%(deepsea_dir), quiet = True).split('\n')[0:-1]
+
+        
+        f_half_score_het_sites(variation_file_list[0], deepsea_dir)
+        
+        half_file_list = my.f_shell_cmd( "find %s/het/ -name '*.diff'"%(deepsea_dir), quiet = True).split('\n')[0:-1]
+        variation_data = pd.read_csv(variation_file_list[0], sep =',', compression= 'gzip').dropna()
+        het_data = pd.read_csv(half_file_list[0], sep =',').dropna()
+
+        print len(my.grep_list('1.1',variation_data.name))
+
+        print variation_data.pivot_table("chr",rows="name",aggfunc=len)
+
+        
+        col_name = 'GM12878|DNase|None'
+        variation_data =variation_data.ix[variation_data.ix[:, col_name]!=0,:]
+
+        print type(variation_data[[col_name]])
+        print type(variation_data.ix[:, col_name])
+        
+        het_data = het_data.ix[het_data.ix[:, col_name]!=0,:]
+        print variation_data.columns
+        print variation_data.shape
+        
+        
+        
+        equal_rows = abs( variation_data.ix[:, col_name] - het_data.ix[:, col_name] ) < 0.0000000000000001
+        homo_rows = variation_data.name == '1|1'
+        assert sum(equal_rows) == sum(homo_rows), 'Homo sites are not eaqual by %s out of %s' %(sum(equal_rows) - sum(homo_rows), len(homo_rows)  )
+
+
+
+
+
+
+
+
     
-    #print het_variation.ix[:,0:7].head()
-    #print variation_data.ix[:,0:7].head()
+if __name__ == "__main__":
+    my.f_ensure_make_dir('%s/het/'%deepsea_dir)
+    
+    suite = unittest.TestLoader().loadTestsFromTestCase( TestDatabaseTable )
+    unittest.TextTestRunner(verbosity=1,stream=sys.stderr).run( suite )
+
+    
+    variation_file_list = my.f_shell_cmd( "find %s -name '*.diff.gz'"%(deepsea_dir), quiet = True).split('\n')[0:-1]
+
+    for loc_variation_file in variation_file_list:
+        f_half_score_het_sites(loc_variation_file, deepsea_dir)
 
