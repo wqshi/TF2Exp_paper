68fa140449c2c1bbd78bcd75570b2fd88efa8920
wqshi <wqshi.nudt@gmail.com>
2016-08-10 11:30:40 -0700

    After add variation_dis to the script. But now I'm not sure I should include it

--- python/p_create_tf_impacting_matrix.py
+++ python/p_create_tf_impacting_matrix.py
@@ -33,25 +33,15 @@ else:
     batch_name = 'test'
 batch_output_dir = f_get_batch_output_dir(batch_name)
 
-tf_dir = '%s/data/raw_data/tf/encode_peaks/' % project_dir
+
 deepsea_dir = '%s/deep_result/%s/%s/evalue/het/' % (batch_output_dir, mode_str ,chr_num)
 
 #deepsea_data = pd.read_csv('%s/infile.head.txt'%deepsea_dir, sep = ',')
-
-peak_list_raw = my.f_shell_cmd( "find %s -name '*gm12878-*.narrowPeak'"%(tf_dir), quiet = True).split('\n')
-
-black_list = my.grep_list(".*(--|Rep[1-9]|-myc|encode-)", peak_list_raw)
-peak_list = list(set(peak_list_raw) - set(['']) - set(black_list))
-
-
-
+peak_list = f_get_tf_peak_list(project_dir)
 
 tf_list = [[re.split('[.]|-',os.path.basename(peak_file))[2], os.path.getsize(peak_file)] for peak_file in peak_list ]
-
 peak_file_df = pd.DataFrame(data = tf_list, columns = ['tf', 'file_size'])
-
 peak_file_df['file_path'] = peak_list
-
 
 ##Keep the large peak file.
 peak_file_df_rmdup = peak_file_df.sort(columns = 'file_size', ascending = False ).drop_duplicates(['tf'])
@@ -60,8 +50,17 @@ print peak_file_df.ix[1:3,:]
 print peak_file_df.ix[1:3,:].sort(columns = 'file_size', ascending = False ).drop_duplicates(['tf'])
 print peak_file_df_rmdup.head()
 
+#the variation distacne to tf. Not all the variants, only the variants near the Tf binding regions.
+variation_dis = pd.read_csv('%s/data/raw_data/wgs/1kg/variant_tf_distance/variation_dis_to_peakMax.%s' % (project_dir, chr_num), sep = '\t',na_values = '.')
+variation_dis.drop_duplicates('start', inplace = True)
+variation_dis['min_dis']=variation_dis.ix[:,4:].apply(np.nanmin, axis = 1)
+
+print len(variation_dis.columns.values[4:-1])
+
+print set(peak_file_df_rmdup.tf) - set(variation_dis.columns.values[4:-1])
 
 
+print variation_dis.shape
 
 
 ############################
@@ -71,17 +70,19 @@ variation_file_list = my.f_shell_cmd( "find %s -name '*.diff'"%(deepsea_dir), qu
 
 variation_data = pd.read_csv(variation_file_list[0], sep =',')
 loc_tf_list = list(set( [ feature.split('|')[1].lower() for feature in my.grep_list('GM12878',variation_data.columns.values)]))
+loc_tf_list.sort()
 my.f_print_list(loc_tf_list)
-
 if batch_name == 'test':
-    loc_tf_list = loc_tf_list[1:5]
-
+    loc_tf_list = ['pol3','c-fos'] + loc_tf_list[1:3]
+    
 logging.info('Number of features %s' % len(loc_tf_list) )
 
 tf_variation_dir = '%s/output/tf_variation/%s/%s' % (batch_output_dir, mode_str, chr_num)
 my.f_ensure_make_dir(tf_variation_dir)
 target_cell = 'gm12878'
 
+def f_convert_deepseaTF_to_peakTF(loc_tf_name):
+    return loc_tf_name.replace('-','').replace('.','')
 
 
 #Manually 
@@ -90,13 +91,32 @@ tf_name_pd.columns = ['tf', 'rename']
 tf_name_pd.to_csv('%s/data/raw_data/tf_name_match.txt'%project_dir, index = False, sep='\t' )
 print tf_name_pd.head()
 
+def f_subet_variation_according_to_tf_distance(variation_data_raw, tf_name, variation_dis):
+    #print ''
+    variation_dis.index  = variation_dis.start.astype(str)
+    variation_data_raw.index = variation_data_raw.start.astype(str)
+    intersect_pos = list(set(variation_dis.index).intersection(set(variation_data_raw.index)))
+    subset_dis=variation_dis.ix[intersect_pos,:]
+    assigned_locations = np.logical_and( subset_dis[tf_name] == subset_dis.min_dis, ~subset_dis.min_dis.isnull())
+    variation_assigned_to_tf = variation_data_raw.ix[ subset_dis.index[assigned_locations],:]
+    return variation_assigned_to_tf
+
+
+
 def parse_one_tf(variation_file_list, peak_file_df_rmdup, target_cell, loc_tf, tf_variation_dir):
     #import ipdb; ipdb.set_trace()
+    loc_peak_tf = f_convert_deepseaTF_to_peakTF(loc_tf)
     try:
-        tf_peak_file = peak_file_df_rmdup.ix[peak_file_df_rmdup.tf == loc_tf.replace('-','').replace('.',''), 'file_path'].tolist()[0]
+        tf_peak_file = peak_file_df_rmdup.ix[peak_file_df_rmdup.tf == loc_peak_tf, 'file_path'].tolist()[0]
     except:
         logging.error("Don't find the peak file for %s " % loc_tf)
         return 0
+    #import ipdb; ipdb.set_trace()
+    if loc_peak_tf not in variation_dis.columns.values:
+        logging.info('Missing %s in variation dis', loc_peak_tf)
+        return 0
+
+    
     print tf_peak_file
     binding_regions = pd.io.parsers.read_csv(tf_peak_file, sep="\t", header=None).ix[:,0:2]
     
@@ -109,13 +129,19 @@ def parse_one_tf(variation_file_list, peak_file_df_rmdup, target_cell, loc_tf, t
     print tf_regions_table.file_path
     
     tf_regions_table.extract_bed()
+
+    
+    #import ipdb; ipdb.set_trace()
     
     for variation_file in variation_file_list:
     
         sample_id = os.path.basename(variation_file).split('.')[0]
-        variation_data = pd.read_csv(variation_file, sep =',')
-        variation_data['start'] = variation_data['pos'].astype(int)
-        variation_data['end'] =  variation_data['pos'].astype(int) + 1
+        variation_data_raw = pd.read_csv(variation_file, sep =',')
+        variation_data_raw['start'] = variation_data_raw['pos'].astype(int) -1
+        variation_data_raw['end'] =  variation_data_raw['pos'].astype(int)
+        variation_data = f_subet_variation_according_to_tf_distance(variation_data_raw, loc_peak_tf, variation_dis)
+        
+        
 
         #print variation_data.ix[:,0:10].head()
 
@@ -156,13 +182,17 @@ def parse_one_tf(variation_file_list, peak_file_df_rmdup, target_cell, loc_tf, t
         os.remove(tf_regions_table.loc_file)
 
 
-for loc_tf in loc_tf_list:
+for loc_tf in loc_tf_list[2:]:
     print loc_tf
     parse_one_tf(variation_file_list, peak_file_df_rmdup, target_cell, loc_tf, tf_variation_dir)
     break 
 
 Parallel(n_jobs=4)(delayed(parse_one_tf)(variation_file_list, peak_file_df_rmdup, target_cell, loc_tf, tf_variation_dir) for loc_tf in loc_tf_list)  
 
+
+
+
+#Delete the half data dir.
 import shutil
 shutil.rmtree(deepsea_dir)
 
