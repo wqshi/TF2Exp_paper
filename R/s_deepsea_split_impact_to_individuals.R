02ec535f20f0790ed983574d193abcfba395e8c9
wqshi <wqshi.nudt@gmail.com>
2016-08-20 09:58:12 -0700

    Add choice of batch_name. Add infer colnames when read data. Write data colname = TRUE.

---
+++ R/s_deepsea_split_impact_to_individuals.R
@@ -0,0 +1,96 @@
+source('~/R/s_function.R', chdir = TRUE)
+library(stringr)
+source('s_project_funcs.R')
+library(foreach)
+library(doMC)
+#registerDoMC(cores=4)
+
+#Input
+#batch_name = '462samples_sailfish'
+batch_name = '800samples'
+
+
+
+###################
+##The rest code##
+
+chr_str = 'chr22'
+
+if (batch_name == '462samples_sailfish'){
+    deepsea_dir = '/homed/home/shi/expression_var/data/462samples_sailfish/deep_result/all/chrMerge2/evalue/'
+}else{
+    deepsea_dir = '/homed/home/shi/expression_var/data/800samples/deep_result/all/chr800/evalue/'
+}
+
+f_split_one_chromosom <- function(chr_str, deepsea_dir, batch_name, test_flag = FALSE){
+    cat('============', chr_str, '=========\n')
+    result_dir = f_p('/homed/home/shi/expression_var/data/%s/deep_result/all/%s/evalue/', batch_name, chr_str)
+    vcf_dir = f_p('/homed/home/shi/expression_var/data/%s/chr_vcf_files/%s/', batch_name, chr_str)
+    dir.create(result_dir, recursive = T)
+    chr_impact = read.table(f_p('%s/%s.diff.gz', deepsea_dir, chr_str), sep = ',', header = T, )
+        
+    #colnames(chr_impact) = chr_impact[1,]
+    #chr_impact = chr_impact[chr_impact$chr != 'chr',]
+    rownames(chr_impact) = paste(chr_impact$chr, chr_impact$pos, chr_impact$ref, chr_impact$alt, sep = ':')
+    head10(chr_impact)
+
+    col_names = str_replace_all(colnames(chr_impact),'None[.][0-9]+$', 'None')
+
+
+    colnames(chr_impact)
+
+    cat('Number of variations in total:', nrow(chr_impact), '\n')
+    head10(chr_impact)
+
+    individual_vcfs_list = grep('vcf.gz', list.files(vcf_dir), value = TRUE)
+    cat(length(individual_vcfs_list), 'individuals', '\n')
+    individual_vcf = individual_vcfs_list[1]
+    if (test_flag == TRUE){
+        individual_vcfs_list=individual_vcfs_list[1:20]
+    }
+    individual_vcf = individual_vcfs_list[2]
+    
+    for (individual_vcf in individual_vcfs_list){
+
+
+        
+        individual_id = str_replace(individual_vcf, '.vcf.gz', '')
+        cat(individual_id, '\n')
+
+        result <- try(
+                        individual_genotype <- read.table(f_p('%s/%s', vcf_dir, individual_vcf), sep = '\t'),
+                        silent=TRUE
+                       )
+    
+        if (class(result) == "try-error"){
+            print(result)
+            cat('Error in ', individual_id, '\n')
+            next
+        }
+
+        individual_genotype = individual_genotype[!duplicated(individual_genotype),]
+       
+        colnames(individual_genotype) = c('chr', 'pos', 'genotype', 'ref', 'alt')
+        rownames(individual_genotype) = with(individual_genotype, paste(chr, pos, ref, alt, sep = ':'))
+        
+        individual_impact = chr_impact[rownames(individual_genotype),]
+        
+    
+        #head10(individual_impact)
+
+        gz1 <- gzfile(f_p("%s/%s.diff.gz", result_dir, individual_id ), "w")
+        write_data = rbind(col_names, individual_impact)
+        write.table(write_data[complete.cases(write_data),], gz1, sep = ',', row.names = FALSE, col.names = TRUE, quote = FALSE)
+        close(gz1)
+    }
+}
+
+f_split_one_chromosom(chr_str, deepsea_dir, batch_name, test_flag = TRUE)
+foreach( chr_str = paste0('chr', c(22:1,'X')) ) %dopar% f_split_one_chromosom(chr_str, deepsea_dir, batch_name)
+
+#foreach( chr_str = paste0('chr', 21:1), .combine=c ) %do% f_split_one_chromosom(chr_str, deepsea_dir)
+#print(results)
+
+
+
+
